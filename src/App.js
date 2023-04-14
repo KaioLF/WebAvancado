2f662e6eec0fcd111b4dcde7765a12b4bdd5c3b9
Kaio <kaio.mlf@hotmail.com>
2023-04-14 20:20:17 -0300

    subindo o código da aula

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonCustom from './ButtonCustom';
+import Toolbar from './Toolbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Toolbar/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aula de eventos e state
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ButtonCustom/>
       </header>
     </div>
   );
